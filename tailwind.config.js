63bd00b667a4674c82e3133daf743ee9a90973d4
Amsurur”git config --global user.name Amsurur” <abdulloevmuhammadsurur@gmail.com>
2023-08-12 22:13:48 +0300

    second

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('home\img\bg-img-1.png')",
+
+      }
+    },
     screens: {
       '2xl': {'max': '1535px'},
       // => @media (max-width: 1535px) { ... }
@@ -19,6 +24,7 @@ module.exports = {
       'sm': {'max': '639px'},
       // => @media (max-width: 639px) { ... }
     }
+
   },
   plugins: [],
 }
