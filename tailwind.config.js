bedbd56aee6ae2492ddc00c45294d3b6ee84e862
Amsurur <138652787+Amsurur@users.noreply.github.com>
2023-08-14 11:27:50 +0300

    Revert "tamom kadum"

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+<<<<<<< HEAD
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('home\img\bg-img-1.png')",
 
+      }
+    },
+=======
+>>>>>>> b11a3ac8c81bbcd49ff4d001f8a61b087e9192a6
     screens: {
       '2xl': { 'max': '1535px' },
       // => @media (max-width: 1535px) { ... }
 
-
+      'xl': { 'max': '1279px' },
       // => @media (max-width: 1279px) { ... }
 
       'lg': { 'max': '1023px' },
@@ -17,11 +25,14 @@ module.exports = {
       // => @media (max-width: 767px) { ... }
 
       'sm': { 'max': '639px' },
-
       // => @media (max-width: 639px) { ... }
+<<<<<<< HEAD
+    }
 
+=======
+    },
     extend: {},
-
+>>>>>>> b11a3ac8c81bbcd49ff4d001f8a61b087e9192a6
   },
   plugins: [],
 };
